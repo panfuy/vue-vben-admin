ecdf36ecae6f8a140544eaf3a4b2dae28e6a329a
PanFu <panfu@zhihuaai.com>
2026-05-13 22:28:53 +0800

    feat: 更改本地配置

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,22 @@
 import { defineConfig } from '@vben/eslint-config';
 
-export default defineConfig();
+export default defineConfig([
+  {
+    files: ['**/*.{js,ts,vue}'],
+    rules: {
+      'no-debugger': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+        'TSEnumDeclaration[const=true]',
+        'TSExportAssignment',
+      ],
+      'vue/no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+  },
+]);
