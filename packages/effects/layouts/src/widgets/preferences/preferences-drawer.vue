526a3b2e20fadb67a85db188a3049c4270c9d3ef
PanFu <panfu@zhihuaai.com>
2026-05-14 11:38:37 +0800

    feat: 快捷键追加ESC控制，关闭当前窗口

--- packages/effects/layouts/src/widgets/preferences/preferences-drawer.vue
+++ packages/effects/layouts/src/widgets/preferences/preferences-drawer.vue
@@ -165,6 +165,7 @@ const shortcutKeysGlobalSearch = defineModel<boolean>(
 const shortcutKeysGlobalLogout = defineModel<boolean>(
   'shortcutKeysGlobalLogout',
 );
+const shortcutKeysGlobalEscape = defineModel<boolean>('shortcutKeysGlobalEscape');
 
 const shortcutKeysGlobalLockScreen = defineModel<boolean>(
   'shortcutKeysGlobalLockScreen',
@@ -518,6 +519,7 @@ function handleCustomPreferencesUpdate(updates: CustomPreferencesRecord) {
                 v-model:shortcut-keys-global-search="shortcutKeysGlobalSearch"
                 v-model:shortcut-keys-lock-screen="shortcutKeysGlobalLockScreen"
                 v-model:shortcut-keys-logout="shortcutKeysGlobalLogout"
+                v-model:shortcut-keys-escape="shortcutKeysGlobalEscape"
               />
             </Block>
           </template>
